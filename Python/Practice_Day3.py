468252fbc39d6ce8d26f62cd3996877f97a8375e
mangeshwahatule <wahatulemangesh07@gmail.com>
2026-09-05 16:03:34 +0530

    Third day

---
+++ Python/Practice_Day3.py
@@ -0,0 +1,73 @@
+# # Loops
+# x = int(input ("Enter a number:-"))
+# nums = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+# i = 0
+# # print(len(numbers))
+
+# while i < len(nums):
+#     if (nums[i] == x):
+#         print("Number found")
+#     i += 1
+
+# For Loop
+# list =(1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+# x = 36
+
+# for num in list:
+#     if num == x:
+#        print("Number found")
+#        break
+# else:
+#     print("not found")
+
+
+#  Range in loops for loop
+# n = int(input("Enter a number:- "))
+# for num in range(1,11):
+#     print(n*num)
+
+# n = 5
+# i = 1
+# sum = 0
+# while i <=n:
+#     sum += i
+#     i+=1
+# print(sum)
+# n = 5
+# fact=1
+# for num in range(n,0,-1):
+#     fact*= num
+# print(fact)
+
+# Function and Recursion 
+# def sum(a, b):
+#      s = a + b
+#      return s
+
+# print(sum(2,3))
+# print(sum(2,3))
+# print(sum(21,3))
+# print(sum(2,-9))
+
+# list =[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# def len_list(list):
+#     print(len(list))
+# len_list(list)
+
+# def list_el(list):
+#     for el in list:
+#         print(el, end=" ")
+# list_el(list)
+# def fact(n):
+#     f = 1
+#     for i in range(1,n+1):
+#         f*=i
+#     return f
+# print(fact(5))
+
+# def cur_converter(n):
+#     inr = n* 94.43
+#     print(n, "USD =", inr, "INR")
+# cur_converter(25)
+
